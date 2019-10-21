11286a0953bca97b8dd4ed017200d0d6ef22658c
Jeverson Luis <51482629+JeversonLuis@users.noreply.github.com>
2019-10-21 12:00:37 -0300

    Add files via upload

---
+++ table.js
@@ -0,0 +1,21 @@
+function gerar(){
+    let txnum = document.querySelector('input#txnum')
+    let tab = document.querySelector('select#seltab')
+
+    if (txnum.value.length == 0) {
+        alert('Please enter a number!')
+    } else {
+        let num = Number(txnum.value)
+        let c = 1
+        tab.innerHTML = '' //Para limpar o select
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
+
